34f1eed7acee19900c838a2afd27ac2542d6f55a
Vinicius Penha <vinicius.penha@icarros.com.br>
2018-11-01 15:53:52 -0300

    ajustes no Swagger

--- src/main/java/br/com/desbravadores/meuclube/config/SwaggerConfig.java
+++ src/main/java/br/com/desbravadores/meuclube/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,13 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.desbravadores.meuclube.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build().apiInfo(new ApiInfo(
+                        "Documentação - API Meu Clube",
+                        null,
+                        "1.0",
+                        null,
+                        new Contact("","",""),
+                        null,
+                        null));
     }
 }
